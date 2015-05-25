7cb9a42312c3dfd46ded6e9e60cad6d726737d3c
root <sunsky303@gmail.com>
2015-05-25 18:31:33 +0800

    fix bg

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if(is_admin() == 1){
     <title>北京大学食堂管理系统</title>
     <style type="text/css" media="screen">
         body{
-background: url(/weimh.jpeg) no-repeat center;
+background: url(/weimh.jpeg) no-repeat center #CCDCFD;
             text-align:center}
         form{
 margin: 150px 100px;
@@ -29,15 +29,19 @@ margin: 150px 100px;
   border-radius: 30px;
   -webkit-box-shadow: 0 0 30px #fff;
         }
-input{
+.str-input{
+      font-size: 14px;
+        padding-left: 20px;
 }
     </style>
 </head>
 <body >
     <form action="./auth_login.php" method="post" accept-charset="utf-8" >
-        <p style="padding-bottom: 30px;;text-align:center">北京大学食堂管理系统</p>
-        <p>用户编号: <input type="text" name="username" value="" size=30></p>
-        <p>用户密码: <input type="password" name="password" value="" size=30></p>
+        <p style="padding: 10px 30px;
+          text-align: center;
+            font-size: 20px;;text-align:center">北京大学食堂管理系统</p>
+        <p class="str-input">用户: <input type="text" name="username" value="" size=30></p>
+        <p class="str-input">密码: <input type="password" name="password" value="" size=30></p>
         <p style="text-align:center"><input type="submit" value="登录"> <input type="reset" value="重置"></p>
     </form>
 </body>
